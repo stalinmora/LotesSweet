d91abd6e27088380090957b1e9ad580915a6d7bd
Stalin Mora <stalin.mora.burgos@gmail.com>
2017-07-11 10:44:39 -0500

    Nueva Ventana de Correcion

---
+++ LotesSweet/com.sweetandcoffee/Entidades/ELotes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.sweetandcoffee.Entidades
+{
+    class ELotes
+    {
+        public int codigo { get; set; }
+        public string Descripcion { get; set; }
+        public float StockLotes { get; set; } 
+        public float Lotes { get; set; }
+    }
+}
